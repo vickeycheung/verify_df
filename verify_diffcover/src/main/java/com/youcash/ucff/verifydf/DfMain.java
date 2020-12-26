165802f334c4a57b38b6b90b33f436d10afb66f9
vickeycheung <13560328059@163.com>
2020-12-27 02:41:21 +0800

    diff m2

--- verify_diffcover/src/main/java/com/youcash/ucff/verifydf/DfMain.java
+++ verify_diffcover/src/main/java/com/youcash/ucff/verifydf/DfMain.java
@@ -14,6 +14,30 @@ public class DfMain {
 		
 	}
 	
+	public void justDoSthWithTest1(){
+		int a = 1;
+		int b = a;
+		
+		System.out.println(a + b);
+		
+		int c = 32;
+		int d = 32;
+		System.out.println(c + d);
+		
+	}
+	
+	public void justDoSthWithTest2(){
+		int a = 1;
+		int b = a;
+		
+		System.out.println(a + b);
+		
+		int c = 32;
+		int d = 32;
+		System.out.println(c + d);
+		
+	}
+	
 	public static void main(String[] args) {
 		DfMain df = new DfMain();
 		df.justDoSthWithTest();
--- verify_m2/src/main/java/com/youcash/ucff/verm2/Dfm2Main.java
+++ verify_m2/src/main/java/com/youcash/ucff/verm2/Dfm2Main.java
@@ -14,6 +14,30 @@ public class Dfm2Main {
 		
 	}
 	
+	public void justDoSthWithTest1(){
+		int a = 1;
+		int b = a;
+		
+		System.out.println(a + b);
+		
+		int c = 32;
+		int d = 32;
+		System.out.println(c + d);
+		
+	}
+	
+	public void justDoSthWithTest2(){
+		int a = 1;
+		int b = a;
+		
+		System.out.println(a + b);
+		
+		int c = 32;
+		int d = 32;
+		System.out.println(c + d);
+		
+	}
+	
 	public static void main(String[] args) {
 		Dfm2Main df = new Dfm2Main();
 		df.justDoSthWithTest();
